3f3e5ed36651f9f2846f270a2cc1c0453c7b206a
Hari Darshan Gorana <er.haridarshan@gmail.com>
2023-03-16 19:13:31 +0530

    fix: minor fix
    Signed-off-by: Hari Darshan Gorana <er.haridarshan@gmail.com>

--- src/Providers/ConsoleServiceProvider.php
+++ src/Providers/ConsoleServiceProvider.php
@@ -3,15 +3,22 @@
 namespace Haridarshan\Laravel\ApiVersioning\Providers;
 
 use Nwidart\Modules\Providers\ConsoleServiceProvider as NwidartConsoleServiceProvider;
+use Haridarshan\Laravel\ApiVersioning\Commands;
 
 class ConsoleServiceProvider extends NwidartConsoleServiceProvider
 {
+    protected array $registerCommands = [
+        Commands\BootstrapMakeCommand::class,
+        Commands\RouteProviderUpdateCommand::class,
+        Commands\ModuleMakeWithVersion::class,
+    ];
+
     public function register(): void
     {
         parent::register();
 
         $this->commands(
-            config('api.commands.register')
+            config('api.commands.register', $this->registerCommands)
         );
     }
 
@@ -20,6 +27,6 @@ class ConsoleServiceProvider extends NwidartConsoleServiceProvider
      */
     public function provides(): array
     {
-        return array_merge(parent::provides(), config('api.commands.register'));
+        return array_merge(parent::provides(), config('api.commands.register', $this->registerCommands));
     }
 }
